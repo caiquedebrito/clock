31dbee692c7a5d4053acfb69809eea446f42582f
Caique De Brito <caiquedebritoo@gmail.com>
2022-07-01 17:32:30 -0300

    create display component and add clock formated

--- src/Display.tsx
+++ src/Display.tsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react"
+
 interface DisplayInterface {
     displayTimeBreak: boolean,
-    timeBreakMinute: string | number,
-    timeBreakSecond: string | number,
-    sessionTimeMinute: string | number,
-    sessionTimeSecond: string | number
+    timeBreakMinute: number,
+    timeBreakSecond: number,
+    sessionTimeMinute: number,
+    sessionTimeSecond:  number
 }
 
-export const Display = ({displayTimeBreak, timeBreakMinute, timeBreakSecond, sessionTimeMinute, sessionTimeSecond}: DisplayInterface) => {
-    return <>
-    {displayTimeBreak ? `${timeBreakMinute}:${timeBreakSecond}`: `${sessionTimeMinute}:${sessionTimeSecond}`}
-    </>
+export const Display = ({
+    displayTimeBreak,
+    timeBreakMinute, 
+    timeBreakSecond, 
+    sessionTimeMinute, 
+    sessionTimeSecond
+}: DisplayInterface) => {
+    const [display, setDisplay] = useState("")
+
+    useEffect(() => {
+        let minutes: string | number
+        let seconds: string | number
+
+        if (displayTimeBreak) {
+            minutes = timeBreakMinute < 10 ? "0" + timeBreakMinute : timeBreakMinute
+            seconds = timeBreakSecond < 10 ? "0" + timeBreakSecond : timeBreakSecond
+        } else {
+            minutes = sessionTimeMinute < 10 ? "0" + sessionTimeMinute : sessionTimeMinute
+            seconds = sessionTimeSecond < 10 ? "0" + sessionTimeSecond : sessionTimeSecond
+        }
+        
+        setDisplay(`${minutes}:${seconds}`)
+
+    }, [sessionTimeMinute, sessionTimeSecond, timeBreakMinute, timeBreakSecond, displayTimeBreak])
+    
+
+    return <div>
+    {
+       display
+    }
+    </div>
 }
